d3a6a6646955151ff40e2f247d12becddb542e6d
yedhrab <yedhrab@gmail.com>
2022-04-08 16:23:53 +0300

    🔖  1.0.1 due to pypi errors

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from os.path import basename, splitext
 
 from setuptools import find_packages, setup
 
-VERSION = "1.0.0"
+VERSION = "1.0.1"
 README_PATH = "docs/README.md"
 
 # test_requirements = ["behave", "behave-classy", "pytest"]
